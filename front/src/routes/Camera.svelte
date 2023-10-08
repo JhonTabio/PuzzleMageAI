9652f92998430dfd6f9cd10b75106881d940fa1d
Dario <dariosans@gmail.com>
2023-10-08 06:02:07 -0400

    added Camera.svelte

---
+++ front/src/routes/Camera.svelte
@@ -0,0 +1,13 @@
+<!-- src/Camera.svelte -->
+<script>
+  export let feedUrl = "";
+</script>
+
+<img src={feedUrl} alt="Camera feed" />
+
+<style>
+  img {
+    width: 100%; /* Adjust as needed */
+    height: auto;
+  }
+</style>
