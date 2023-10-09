6a16f9fbd3bc60daf5993bcf9f171e5e3894099b
Matthew Labrada <61965456+MatthewLabrada@users.noreply.github.com>
2023-10-08 20:41:56 -0400

    removed atlas connection string

--- mongodb/mongodbtest.py
+++ mongodb/mongodbtest.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://matthewlabrada:Hackathong@cluster0.hvtyobg.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
+uri = "ENTER_ATLAS_CONNECTION_STRING_HERE"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
