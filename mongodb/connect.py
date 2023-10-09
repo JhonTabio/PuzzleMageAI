17c96a26ade0585a6c835078e167f7b5c1fae81c
Matthew Labrada <61965456+MatthewLabrada@users.noreply.github.com>
2023-10-08 20:41:17 -0400

    removed atlas connection string

--- mongodb/connect.py
+++ mongodb/connect.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 def connect(db_name, collection_name):
     # MongoDB connection settings
-    uri = "mongodb+srv://matthewlabrada:Hackathong@cluster0.hvtyobg.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
+    uri = "ENTER_ATLAS_CONNECTION_STRING_HERE"
     client = MongoClient(uri)
     db = client[str(db_name)]
     collection = db[str(collection_name)]
